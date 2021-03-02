1d7d2aa53784ac4a1669c0b93f38fc60c5208bf1
JPGmooder <jpgmooder@gmail.com>
2021-03-02 11:39:11 +0300

    11

--- Spells.h
+++ Spells.h
@@ -4,12 +4,44 @@
 
 #ifndef MAIN_CPP_SPELLS_H
 #define MAIN_CPP_SPELLS_H
-
-
+#include <iostream>
+using namespace std;
 class Spells {
 
 private:
-    int
+    string Name; // Название
+    string Type; // Тип (дамажный, антимагический, усиляющий, разум).
+    string Cost; // Цена в мане
+    string Disctance; // Дистанция заклинания (Один/ауе/все персонажи).
+    string Level; // Уровень заклинания.
+    string Stih; // Стихия заклинания (Всего 4 - Земля, Огонь, Вода, Воздух).
+    string ALLSPELLS[16][7] = {
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+
+    }; // инициализация всех перков
+
+public:
+    void SpellADD (string name) // Добавление заклинание через массив данных о заклинаниях.
+    {
+
+    }
+
+
 };
 
 
