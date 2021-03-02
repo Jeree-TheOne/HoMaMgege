9539ee98f4a1b42daafc1c44a3d90c405c504def
JPGmooder <jpgmooder@gmail.com>
2021-03-02 10:58:55 +0300

    11

---
+++ Spells.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kosty on 02.03.2021.
+//
+
+#include "Spells.h"
---
+++ Spells.h
@@ -0,0 +1,16 @@
+//
+// Created by kosty on 02.03.2021.
+//
+
+#ifndef MAIN_CPP_SPELLS_H
+#define MAIN_CPP_SPELLS_H
+
+
+class Spells {
+
+private:
+    int
+};
+
+
+#endif //MAIN_CPP_SPELLS_H
