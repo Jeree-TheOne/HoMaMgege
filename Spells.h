063873fad2ab7eb2f0dbb5903a6d0edeaebc006b
JPGmooder <jpgmooder@gmail.com>
2021-03-04 13:25:42 +0300

    11

--- Spells.h
+++ Spells.h
@@ -9,7 +9,7 @@ using namespace std;
 class Spells {
 
 protected:
-    string Name; // Название
+    int Name; // Название
     string Stih; // Стихия заклинания (Всего 4 - Земля, Огонь, Вода, Воздух).
     string Type; // Тип (дамажный, антимагический, усиляющий, разум).
     string Distance; // Дистанция заклинания (Один/ауе/все персонажи).
@@ -17,47 +17,47 @@ protected:
     string Level; // Уровень заклинания.
     string Cost; // Цена в маны
 public:
-    Spells *spell [16];
     // (1 - дамаг, 2 - Разум, 3 - баф/дебаф, 4 -  магия/антимагия)
     // Огненные *Fireball(фаерболл), *Blind(Слепота), *Curse(Проклятие), *Fireshield(Огненный щит);
     // Водные *Icearrow(ледянная стрела), *Clone(Клон), *Wikness(Слабость), *Waterdefence(Защита от магии воды);
     // Земля *Boom(Взрыв), *UndeadDead(Упокоить мертвецов), *StoneSkin(Каменная кожа), *DirtDefence(Защита от магии земли);
     // Воздух. *Сhainzipper(Цепная молния), *Accuracy(Точность), *Lucky(Удача), *Dimensiondoor(Дверь измерений)
+     static Spells Clone, Weakness, Waterdefence, Blind, Curse, Fireshield, UndeadDead, StoneSkin, DirtDefence, Accuracy, Lucky, Dimensiondoor, Fireball, Icearrow, boom, Chainzipper;
+
+    void SetSpell(int Name, string Stih, string Type, string Distance, string Duration, string Level, string Cost)
+    {
+        this->Name = Name;
+        this->Stih = Stih;
+        this->Type = Type;
+        this->Distance = Distance;
+        this->Duration = Duration;
+        this->Level = Level;
+        this->Cost = Cost;
+    }
+
+   enum spellsname
+   {
+        NClone,
+        NWeakness,
+        NWaterdefence,
+        NBlind,
+        NCurse,
+        NFireshield,
+        NUndeadDead,
+        NStoneSkin,
+        NDirtDefence,
+        NAccuracy,
+        NLucky,
+        NDimensiondoor,
+        NFireball,
+        NIcearrow,
+        Nboom,
+        NChainzipper
+   };
+
 
 };
-class DSpell : public Spells
-{
-private: int Damage; // Дамаг заклинания
-public:
-    void SetDSpell(string name, string stih, string type, string distance, string level, string cost, string duration, int damage){
-        Name = name;
-        Stih = stih;
-        Type = "D";
-        Distance = distance;
-        Level = level;
-        Cost = cost;
-        Duration = duration;
-        Damage = damage;
-    }
-   DSpell Fireball(), Icearrow(), boom(), Chainzipper();
-}; // Дамажные заклинания
-class SSpell : public Spells
-{
-private: string Effect; // Эффект заклинания
-public:
-    void SetSpell(string name, string stih, string type, string distance, string level, string cost, string duration, string effect)
-    {
-        Name = name;
-        Stih = stih;
-        Type = type;
-        Distance = distance;
-        Level = level;
-        Cost = cost;
-        Duration = duration;
-        Effect = effect;
-    }
-    SSpell Clone(), Wikness(), Waterdefence(), Blind(), Curse(), Fireshield(), UndeadDead(), StoneSkin(), DirtDefence(), Accuracy(), Lucky(), Dimensiondoor();
-};
+
 
 
 
