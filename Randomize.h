92ba8030fdd28ac21280ee69bcd4dc7523e5502e
JPGmooder <jpgmooder@gmail.com>
2021-03-04 19:40:12 +0300

    11

--- Randomize.h
+++ Randomize.h
@@ -5,6 +5,7 @@
 #ifndef MAIN_CPP_RANDOMIZE_H
 #define MAIN_CPP_RANDOMIZE_H
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 class Randomize {
@@ -13,6 +14,7 @@ public:
     {
         static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
         // Равномерно распределяем рандомное число в нашем диапазоне
+        srand((unsigned) time(0));
         return static_cast<int>(rand() * fraction * (max - min + 1) + min);
     }
 };
