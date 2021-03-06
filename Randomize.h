58060b495b95e3de0c9f1eae9c89af04f4865a0a
Alex <78584746+Jeree-TheOne@users.noreply.github.com>
2021-03-06 21:35:10 +0300

    NahalChanges0.2

--- Randomize.h
+++ Randomize.h
@@ -5,16 +5,19 @@
 #ifndef MAIN_CPP_RANDOMIZE_H
 #define MAIN_CPP_RANDOMIZE_H
 #include <iostream>
+#include <random>
 #include <time.h>
 using namespace std;
 
 class Randomize {
 public:
-    int Rand(int min, int max)
+    static int GetRand(int min, int max)
     {
-        srand(static_cast<unsigned int>(time(0)));
-        static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
-        return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+        std::random_device RandomDevice;
+        std::mt19937 generator(RandomDevice());
+        std::uniform_int_distribution<> distribution(min, max);
+        int x = distribution(generator);
+        return x;
     }
 };
 
